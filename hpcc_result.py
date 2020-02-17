2c90fd08773c225ee2b8158786e29c8af6901fb1
tings0802 <nick19980922@gmail.com>
2020-02-17 22:13:22 +0800

    update

--- hpcc_result.py
+++ hpcc_result.py
@@ -4,32 +4,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-openFileName = '10000N_4proc_4NB'
-with open(openFileName, 'r') as ifile:
-    filename = ifile.name
-    raw = ifile.readlines()
+path = '..\\hpcc_results\\'
+filelist = os.listdir(path)
 
-startLine = 'Begin of Summary section.'
-endLine = 'End of Summary section.'
-startLinePos = 0
-endLinePos = 0
-
-while startLine not in raw[startLinePos]:
-    startLinePos += 1
-#startLinePos += 1
-
-while endLine not in raw[endLinePos]:
-    endLinePos += 1
-#endLinePos -= 1
-
-result = {}
-for i in range(startLinePos+1,endLinePos):
-    res = raw[i].split('=')
-    res[1] = res[1].strip()
-    result[res[0]] = res[1]
-
-resultTitle = ['HPL_N', 'HPL_NB',
+resultTitle = ['HPL_N',
+               'HPL_NB',
                'HPL_Tflops',
                'PTRANS_GBs', 
                'MPIRandomAccess_GUPs', 
@@ -38,9 +19,42 @@ resultTitle = ['HPL_N', 'HPL_NB',
                'StarDGEMM_Gflops', 
                'RandomlyOrderedRingBandwidth_GBytes', 
                'RandomlyOrderedRingLatency_usec']
-resultData = []
-for i in resultTitle:
-    resultData.append(result[i])
-df = pd.DataFrame(np.array([resultData]), columns = resultTitle)
 
-df.to_excel('result.xlsx', sheet_name='Sheet1', index=False)
+allData = pd.DataFrame(columns = resultTitle)
+
+for file in filelist:
+    with open(path + file, 'r') as ifile:
+        raw = ifile.readlines()
+
+    startLine = 'Begin of Summary section.'
+    endLine = 'End of Summary section.'
+    startLinePos = 0
+    endLinePos = 0
+
+    while startLine not in raw[startLinePos]:
+        startLinePos += 1
+
+    while endLine not in raw[endLinePos]:
+        endLinePos += 1
+
+    result = {}
+    for i in range(startLinePos + 1, endLinePos):
+        res = raw[i].split('=')
+        res[1] = res[1].strip()
+        result[res[0]] = res[1]
+    
+    resultData = []
+    for i in resultTitle:
+        resultData.append(result[i])
+    df = pd.DataFrame(np.array([resultData]), columns = resultTitle)
+    
+    allData = allData.append(df, ignore_index=True)
+
+for col in resultTitle[0:2]:
+    allData[col] = allData[col].astype(int)
+for col in resultTitle[2:]:
+    allData[col] = allData[col].astype(float)
+allData.dtypes
+
+allData.to_excel('result.xlsx', sheet_name='Sheet1', index=False)
+
