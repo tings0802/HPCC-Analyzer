909632ba8e183b1d50ebf63cf56f9bc94ba26d80
tings0802 <nick19980922@gmail.com>
2020-04-24 22:50:49 +0800

    center of mass completed

---
+++ namd/cmass.py
@@ -0,0 +1,157 @@
+def readPDB(pfile):
+    ''' read a pdb file and return coordinates of atoms (2D list) '''
+    atomicCoor = []     # [[name, x, y, z]]
+
+    with open(pfile, 'r') as ifile:
+        rawData = ifile.readlines()
+    
+    for row in rawData:
+        data = row.split()
+        if data[0] == 'ATOM':
+            atomicCoor.append([row[13], float(data[6]), float(data[7]), float(data[8])])
+    
+    return atomicCoor
+
+def readTOP(tfile):
+    ''' read a topology file and return atomic masses (dict) '''
+    atomicMass = {}     # {name: mass}
+ 
+    with open(tfile, 'r') as ifile:
+        rawData = ifile.readlines()
+
+    for row in rawData:
+        data = row.split()
+        try:
+            if data[0] == 'MASS':
+                atomicMass[data[4]] = float(data[3])
+        except:
+            pass
+        
+    return atomicMass
+
+def readTOPs(tdir='./topology'):
+    ''' read topology files in tdir and return atomic masses (dict) '''
+    atomicMass = {}     # {name: mass}
+
+    if tdir[-1] != '/' or '\\':
+        tdir += '/'
+    
+    import os
+    for tfile in os.listdir(tdir):
+        atomicMass = {**atomicMass, **readTOP(tdir + tfile)}
+        
+    return atomicMass
+
+#############################################
+
+def createMole(atomicCoor, atomicMass):
+    ''' merge atomic mass into coordinates list '''
+    molecule = atomicCoor   # [[name, x, y, z, mass]]
+    missing = []            # [[name, x, y, z, index]]
+
+    for i in range(len(molecule)):
+        atom = molecule[i]
+        try:
+            atom.append(atomicMass[atom[0]])
+        except:
+            missing.append([*atom, i])
+            atom.append(0)
+
+    return molecule, missing
+
+def calulateCM(molecule):   # [[name, x, y, z, mass]]
+    ''' calculate the molecule's center of mass and molecular mass'''
+    cenMass = [0, 0, 0]     # [x, y, z]
+    totMass = 0
+
+    for atom in molecule:
+        totMass += atom[4]
+        for i in range(len(cenMass)):
+            cenMass[i] += atom[i + 1] * atom[4]
+    
+    for i in range(len(cenMass)):
+        cenMass[i] /= totMass
+    
+    return cenMass, totMass
+
+#############################################
+
+def printList(List):
+    ''' print 2D list '''
+    for line in List:
+        for data in line:
+            print(data, end='\t')
+        print()
+
+def printDict(Dict):
+    ''' print key-value pairs line by line '''
+    for key in Dict.keys():
+        print(f'{key}\t{Dict[key]}')
+
+def printMole(molecule, limit=-1):    # [[name, x, y, z, mass]]
+    ''' print the atoms list of a molecule by the given format '''
+    print(f'name\tmass\t\tcoordinates')
+    for atom in molecule:
+        print(f'{atom[0]}\t{atom[4]}\t\t{atom[1:4]}')
+        limit -= 1
+        if not limit: break
+    print(f'length: {len(molecule)} atoms')
+
+def printMiss(missing, limit=-1):     # [[name, x, y, z, index]]
+    ''' print the missing atoms in a molecule '''
+    if missing:
+        print(f'name\tindex\tcoordinates')
+        for atom in missing:
+            print(f'{atom[0]}\t{atom[4]}\t{atom[1:4]}')
+            limit -= 1
+            if not limit: break
+    print(f'missing: {len(missing)} atoms')
+
+def printCM(cenMass, totMass=0, precision=4):
+    ''' print center of mass and molecular mass with given presicion '''
+    form = f'.{precision}f'
+    print(f'center of mass: [{cenMass[0]:{form}}, {cenMass[1]:{form}}, {cenMass[2]:{form}}]')
+    if totMass: print(f'molecular mass: {totMass:{form}}')
+
+#############################################
+
+def testPrint():
+    printList([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print()
+    printDict({'A': 4, 'B': 5, 'C': 6})
+    print()
+    printMole([[1, 4, 5, 3, 2], [3, 5, 2, 0, 8], [7, 1, 2, 4, 9]])
+
+def testTOP(tdir='./topology/', tfile='top_all22_prot.rtf'):
+    print(f'read a single topology file: {tdir + tfile}')
+    printDict(readTOP(tdir + tfile))
+    print(f'\nread topology files in directory: {tdir}')
+    printDict(readTOPs(tdir))
+
+def testPDB(pdir='./pdb/', pfile='receptor5_Pemirolast.pdb'):
+    printList(readPDB(pdir + pfile))
+
+def testMole():
+    coor = readPDB('./pdb/receptor5_Pemirolast.pdb')
+    mass = readTOPs('./topology/')
+    molecule, missing = createMole(coor, mass)
+
+    printMole(molecule, 18)
+    print()
+    printMiss(missing)
+
+def testCM():
+    coor = readPDB('./pdb/receptor5_Pemirolast.pdb')
+    mass = readTOPs('./topology/')
+    molecule, missing = createMole(coor, mass)
+    cenMass, totMass = calulateCM(molecule)
+
+    printCM(cenMass, totMass)
+
+#############################################
+
+if __name__ == "__main__":
+    testTOP()
+    testPDB()
+    testMole()
+    testCM()
